b486fd0763b4e93c09d4fed2df5c1a031e4c90fd
arthurto <arthur.ps22009@gmail.com>
2022-08-25 18:52:28 -0300

    projects and users streams added

---
+++ tap_linear/queries/users.py
@@ -0,0 +1,44 @@
+
+usersQuery = """
+        query Users($next: String, $replicationKeyValue: DateTime) {
+						users(
+							first: 100
+							after: $next
+							filter: { updatedAt: {gt: $replicationKeyValue } }
+						) {
+							pageInfo {
+								hasNextPage
+								endCursor
+							}
+							nodes {
+								id
+								name
+								active
+								admin
+                                archivedAt
+                                avatarUrl
+                                calendarHash
+								createdAt
+                                createdIssueCount
+								description
+                                disableReason
+								displayName
+                                inviteHash
+                                isMe
+								email
+								guest
+								lastSeen
+								organization {
+									id
+									name
+								}
+								statusLabel
+								timezone
+								updatedAt
+								url
+                                statusEmoji
+                                statusUntilAt
+							}
+						}
+					}
+        """
--- tap_linear/streams.py
+++ tap_linear/streams.py
@@ -1,15 +1,35 @@
 """Stream type classes for tap-linear."""
 from tap_linear.client import LinearStream
+
 from tap_linear.schemas.issues import issuesSchema
+from tap_linear.schemas.projects import projectsSchema
+from tap_linear.schemas.users import usersSchema
+
 from tap_linear.queries.issues import issuesQuery
+from tap_linear.queries.projects import projectsQuery
+from tap_linear.queries.users import usersQuery
 
 
 class IssuesStream(LinearStream):
 
-    """Define custom stream."""
-
-    name = "Issues"
+    name = "issues"
     schema = issuesSchema
     primary_keys = ["id"]
     replication_key = "updatedAt"
     query = issuesQuery
+
+class ProjectsStream(LinearStream):
+
+    name = "projects"
+    schema = projectsSchema
+    primary_keys = ["id"]
+    replication_key = "updatedAt"
+    query = projectsQuery
+
+class UsersStream(LinearStream):
+
+    name = "users"
+    schema = usersSchema
+    primary_keys = ["id"]
+    replication_key = "updatedAt"
+    query = usersQuery
